8e997bb339cba5f6054a5735f8f21ab4461dad57
lseelig <37001137+LSeelig@users.noreply.github.com>
2024-09-16 09:43:32 -0500

    Implement play form for login

--- server/app/controllers/Messages1.scala
+++ server/app/controllers/Messages1.scala
@@ -4,15 +4,23 @@ import javax.inject._
 
 import shared.SharedMessages
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+
+case class LoginData(username: String, password: String)
 
 @Singleton
-class Messages1 @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
-  
+class Messages1 @Inject()(cc: MessagesControllerComponents) extends MessagesAbstractController(cc) {
+  val loginForm = Form(mapping(
+    "Username" -> text(3, 10),
+    "Password" -> text(8)
+    )(LoginData.apply)(LoginData.unapply))
+
   // Login, main page still reachable without redirect if browser goes "back" after signing in, but actions are blocked as "unauthorized"
 
   def login = Action { implicit request => 
     if (request.session.get("username").isDefined) Redirect(routes.Messages1.messages)
-    else Ok(views.html.login1())
+    else Ok(views.html.login1(loginForm))
   }
 
   def validateLogin = Action { implicit request =>
@@ -26,6 +34,18 @@ class Messages1 @Inject()(cc: ControllerComponents) extends AbstractController(c
         Redirect(routes.Messages1.login).flashing("error" -> "Invalid username/password combination.")
       }
     }.getOrElse(Redirect(routes.Messages1.login))
+  }
+
+  def validateLoginForm = Action { implicit request =>
+    loginForm.bindFromRequest.fold(
+      formWithError =>BadRequest(views.html.login1(formWithError)),
+      ld => 
+        if (models.MessagesInMemoryModel.validateUser(ld.username, ld.password)) {
+          Redirect(routes.Messages1.messages).withSession("username" -> ld.username)
+        } else {
+          Redirect(routes.Messages1.login).flashing("error" -> "Invalid username/password combination.")
+        }
+    )
   }
 
   def createUser = Action { implicit request =>
